0cbe6e786ba329ddd0b4f3c1c772efb9799f31c8
Adriano Raiano <adriano@raiano.ch>
2016-05-27 14:29:31 +0200

    leak and clean command

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var crypto = require('crypto'),
   http = require('http'),
   socketIO = require('socket.io');
 
+var gc = (require('gc-stats'))();
+var usage = require('usage');
+
 function generateKey() {
   var sha = crypto.createHash('sha256');
   sha.update(Math.random().toString());
@@ -29,21 +32,47 @@ var app = http.createServer(function (req, res) {
 
 var io = socketIO(app);
 
+var pid = process.pid;
+
 io.sockets.use(function (socket, next) {
   console.log(process.env.PORT + ': headers: ');
   console.log(socket.handshake.headers);
   next();
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
+  var id = generateKey();
+
+  gc.on('stats', function (info) {
+    console.log(JSON.stringify(info, null, 2));
+    usage.lookup(pid, function(err, result) {
+      console.log(JSON.stringify(result, null, 2));
+      io.emit('chat message', JSON.stringify(result, null, 2));
+    });
+  });
+
   console.log('connected');
   socket.on('chat message', function(msg){
-    console.log(process.env.PORT);
     console.log(msg)
+
+    if (msg === 'leak') {
+      io.emit('chat message', 'ok... I will leak');
+      for (var i = 0; i < 10000; i++) {
+        memoryLeak[id] = memoryLeak[id] || {};
+        memoryLeak[id][generateKey()] = generateKey();
+      }
+      return;
+    }
+
+    if (msg === 'clear' || msg === 'clean') {
+      io.emit('chat message', 'ok... I will clean the leak');
+      delete memoryLeak[id];
+      return;
+    }
+
     io.emit('chat message', msg);
   });
   socket.on('disconnect', function (reason) {
-    console.log(process.env.PORT);
     console.log('socket disconnected: ' + reason);
   });
 });
